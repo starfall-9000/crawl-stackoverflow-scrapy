e252dc0abc5902bef426dbddc7165c8b262c8999
anvu-trustcircle <an.vu@trustcircle.com>
2018-05-16 10:55:11 +0700

    add multi link crawl

--- beat/beat/spiders/get_posts_by_slug.py
+++ beat/beat/spiders/get_posts_by_slug.py
@@ -5,10 +5,16 @@ import pdb
 
 from beat.items import BeatItem
 
+def get_start_urls():
+    listIndex = [i*5 for i in range(10)]    # [0, 5, ... , 45]
+    format_url = 'http://api.beat.vn/api/post/api_get_posts_by_slug/nong/{0}/5?device_id=pc&token='
+    start_urls = [format_url.format(index) for index in listIndex]
+    return start_urls
+
 class GetPostsBySlugSpider(scrapy.Spider):
     name = 'get_posts_by_slug'
     allowed_domains = ['api.beat.vn']
-    start_urls = ['http://api.beat.vn/api/post/api_get_posts_by_slug/nong/0/5?device_id=pc&token=']
+    start_urls = get_start_urls()
 
     def parse(self, response):
         print("Process.." + response.url)
